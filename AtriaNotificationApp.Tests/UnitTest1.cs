8a4e12dc79f3fe99762e1d14aadc59d83ea4658c
Lakhan Soren <lakhansrn@gmail.com>
2018-07-22 15:28:54 +0530

    Added test

---
+++ AtriaNotificationApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AtriaNotificationApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

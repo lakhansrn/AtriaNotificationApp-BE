1161623a4ff2342d456ffd8f49854cdf6b89fff5
yusoofash <41569614+yusoofash@users.noreply.github.com>
2019-04-28 23:20:30 +0530

    push notification api created

---
+++ AtriaNotificationApp.API/Models/NotificationModelDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtriaNotificationApp.API.Models
+{
+    public class NotificationModelDto
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
